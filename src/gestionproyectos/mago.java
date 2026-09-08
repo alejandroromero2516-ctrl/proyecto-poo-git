eb2fad2ec09d3c8d87618b1115b80bb6a39e1e56
Luis Alejandro Romero Suarez <laromeros@udistrital.edu.co>
2026-09-08 06:44:23 -0500

    feat: añade metodo lanzarHechizo a mago

--- src/gestionproyectos/mago.java
+++ src/gestionproyectos/mago.java
@@ -1,10 +1,12 @@
 package gestionproyectos;
 
 public class Mago extends Tarea {
+    // Atributos de la clase Mago
     private String nombreHechizo;
     private int mana;
     private int nivelPoder;
 
+    // Constructor
     public Mago(String nombre, String nombreHechizo, int mana, int nivelPoder) {
         super(nombre);
         this.nombreHechizo = nombreHechizo;
@@ -12,5 +14,14 @@ public class Mago extends Tarea {
         this.nivelPoder = nivelPoder;
     }
 
-    
+    // Método 1: Lanzar hechizo
+    public void lanzarHechizo() {
+        System.out.println("El mago lanza el hechizo: " + nombreHechizo);
+    }
+
+    // Método 2: Meditar
+    public void meditar() {
+        this.mana += 20;
+        System.out.println("El mago medita y recupera mana.");
+    }
 }
