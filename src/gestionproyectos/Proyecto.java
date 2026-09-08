cff4cbd3538c7ef4a37f28f14df200155fa04751
Luis Alejandro Romero Suarez <laromeros@udistrital.edu.co>
2026-09-08 06:14:45 -0500

    feat: agrega metodo calcularAvance a Proyecto

---
+++ src/gestionproyectos/Proyecto.java
@@ -0,0 +1,14 @@
+package gestionproyectos;
+
+public class Proyecto {
+    private String titulo;
+
+    public Proyecto(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public double calcularAvance() {
+        // Lógica temporal para calcular el avance
+        return 0.0;
+    }
+}
